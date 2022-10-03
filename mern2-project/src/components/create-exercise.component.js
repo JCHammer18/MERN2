8cca2aba99720e944b5680d0c59424acf069adb1
Charlotte Strickland <charlottemarie81@gmail.com>
2022-10-02 22:40:37 -0400

    Revert "added correct code"
    This reverts commit f1cb5da8fa9db266bbabf5081cfbedbd55de4210.

--- mern2-project/src/components/create-exercise.component.js
+++ mern2-project/src/components/create-exercise.component.js
@@ -1,62 +1,10 @@
 import React, { Component } from 'react';
 
-export default class CreateExercise extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            username: '',
-            description: '',
-            duration: 0,
-            date: new Date(),
-            users: []
-        }
-    }
-
-    onChangeUsername(e) {
-        this.setState({
-            username: e.target.value
-        });
-    }
-
-    onChangeDescription(e) {
-        this.setState({
-            description: e.target.value
-        });
-    }
-
-    onChangeDuration(e) {
-        this.setState({
-            duration: e.target.value
-        });
-    }
-
-    onChangeDate(date) {
-        this.setState({
-            date: date
-        });
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-
-        const exercise = {
-            username: this.state.username,
-            description: this.state.description,
-            duration: this.state.duration,
-            date: this.state.date
-        }
-        
-        console.log(exercise);
-
-        window.location = '/';
-    }
-
-
+export default class ExercisesList extends Component {
   render() {
     return (
       <div>
-        <p>You are on the Create Excercises component!</p>
+        <p>You are on the Exercises List component!</p>
       </div>
     )
   }
